6eec1df1231ea78b8b05279341b404df197c1310
Espiritu <diegoesp1011@gmail.com>
2026-02-22 23:23:55 -0700

    Update variables in memory after saving to file
    Add variable updates in current process memory after sed commands.

--- dhcp.sh
+++ dhcp.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Capturar la ruta real de este archivo al momento del source.
+# Necesario porque cuando main.sh hace "source dhcp.sh",
+# $0 apunta a main.sh y los sed -i guardarian en el archivo equivocado.
 DHCP_FILE="$(realpath "${BASH_SOURCE[0]}")"
 
 source "$(dirname "$DHCP_FILE")/funciones_comunes.sh"
@@ -228,6 +231,17 @@ dhcp_conf_parametros(){
 	sed -i "s/^DNS2=.*/DNS2=$DNS2_T/"              "$DHCP_FILE"
 	sed -i "s/^LEASE=.*/LEASE=$LEASE_T/"           "$DHCP_FILE"
 	sed -i "s/^MASCARA=.*/MASCARA=$MASCARA_T/"     "$DHCP_FILE"
+
+	# Actualizar variables en memoria del proceso actual
+	# sed -i solo guarda en el archivo, no actualiza las variables en uso
+	SCOPE="$SCOPE_T"
+	IPINICIAL="$INICIAL_T"
+	IPFINAL="$FINAL_T"
+	GATEWAY="$GATEWAY_T"
+	DNS="$DNS_T"
+	DNS2="$DNS2_T"
+	LEASE="$LEASE_T"
+	MASCARA="$MASCARA_T"
 
 	echo ""
 	echo "Parametros guardados correctamente."
