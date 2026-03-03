c5a52fc76d75bd61dd1cf36b1dcbe0aa6a01b5c5
Espiritu <diegoesp1011@gmail.com>
2026-03-03 01:03:03 -0700

    Update cosa.ps1

--- cosa.ps1
+++ cosa.ps1
@@ -1,22 +1,20 @@
 Import-Module WebAdministration
 
-# Corregir el protocolo a ftp
-Set-ItemProperty "IIS:\Sites\FTP-Servidor" -Name enabledProtocols -Value "ftp"
+# Ver el valor actual
+Get-WebConfigurationProperty -PSPath "IIS:\" -Location "FTP-Servidor" -Filter "system.ftpServer/security/authentication" -Name "."
 
-# Verificar el modo de aislamiento
-Set-ItemProperty "IIS:\Sites\FTP-Servidor" -Name ftpServer.userIsolation.mode -Value 3
+# Forma alternativa de establecer el aislamiento
+$sitio = "FTP-Servidor"
+Set-WebConfigurationProperty `
+    -PSPath "IIS:\" `
+    -Location $sitio `
+    -Filter "system.ftpServer/userIsolation" `
+    -Name "mode" `
+    -Value "IsolateUsers"
 
-# Verificar autenticacion
-Set-ItemProperty "IIS:\Sites\FTP-Servidor" -Name ftpServer.security.authentication.anonymousAuthentication.enabled -Value $true
-Set-ItemProperty "IIS:\Sites\FTP-Servidor" -Name ftpServer.security.authentication.basicAuthentication.enabled -Value $true
-
-# Sin SSL
-Set-ItemProperty "IIS:\Sites\FTP-Servidor" -Name ftpServer.security.ssl.controlChannelPolicy -Value 0
-Set-ItemProperty "IIS:\Sites\FTP-Servidor" -Name ftpServer.security.ssl.dataChannelPolicy -Value 0
-
-Restart-Service FTPSVC -Force
-Start-Sleep -Seconds 2
-
-# Confirmar
-(Get-ItemProperty "IIS:\Sites\FTP-Servidor").enabledProtocols
-(Get-ItemProperty "IIS:\Sites\FTP-Servidor" -Name ftpServer.userIsolation.mode).Value
+# Verificar
+Get-WebConfigurationProperty `
+    -PSPath "IIS:\" `
+    -Location $sitio `
+    -Filter "system.ftpServer/userIsolation" `
+    -Name "mode"
