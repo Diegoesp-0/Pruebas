87b78af6bc19d10b79d9850e9ad8f021074e7cc6
Espiritu <diegoesp1011@gmail.com>
2026-03-03 00:56:19 -0700

    Update cosa.ps1

--- cosa.ps1
+++ cosa.ps1
@@ -1,12 +1,3 @@
-$usuario = "diego"
-
-# Este es el permiso critico que falta - el usuario debe ser dueno de su propia raiz
-icacls "C:\FTP\LocalUser\$usuario" /setowner $usuario /T
-icacls "C:\FTP\LocalUser\$usuario" /grant "${usuario}:(OI)(CI)F" /T
-
-# Tambien en la raiz superior
-icacls "C:\FTP" /grant "${usuario}:(RX)"
-icacls "C:\FTP\LocalUser" /grant "${usuario}:(RX)"
-
-Restart-Service FTPSVC -Force
-Start-WebSite -Name "FTP-Servidor"
+# Ver que sitios existen en IIS
+Import-Module WebAdministration
+Get-WebSite
