86a9f602324628c400d80cd303f1eb4666161344
Espiritu <diegoesp1011@gmail.com>
2026-03-03 01:14:48 -0700

    Update cosa.ps1

--- cosa.ps1
+++ cosa.ps1
@@ -1,1 +1,6 @@
-dir "C:\Windows\System32\inetsrv\config\backup"
+$configPath = "C:\Windows\System32\inetsrv\config\applicationHost.config"
+$content = Get-Content $configPath -Raw -Encoding UTF8
+
+# Mostrar la seccion del sitio FTP
+$match = [regex]::Match($content, '<site name="FTP-Servidor".*?</site>', [System.Text.RegularExpressions.RegexOptions]::Singleline)
+$match.Value
