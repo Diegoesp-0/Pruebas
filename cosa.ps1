2c441fde73b21518ad65cee0b400da0f03e0b75c
Espiritu <diegoesp1011@gmail.com>
2026-03-03 01:01:07 -0700

    Update cosa.ps1

--- cosa.ps1
+++ cosa.ps1
@@ -1,3 +1,13 @@
-net localgroup Usuarios diego /add
-net localgroup Usuarios diego
-Restart-Service FTPSVC -Force
+# Ver el log de errores de IIS FTP en tiempo real
+Get-EventLog -LogName System -Source "*ftpsvc*" -Newest 10
+
+# Y tambien
+Get-EventLog -LogName Application -Newest 20 | Where-Object {$_.Message -like "*ftp*" -or $_.Message -like "*diego*"}
+
+Import-Module WebAdministration
+
+# Ver configuracion de aislamiento
+Get-ItemProperty "IIS:\Sites\FTP-Servidor" | Select-Object *
+
+# Ver el modo de aislamiento especificamente  
+(Get-ItemProperty "IIS:\Sites\FTP-Servidor" -Name ftpServer.userIsolation.mode).Value
