7c02db8ed9b287de6d056e1f1b3fe77638e90631
Espiritu <diegoesp1011@gmail.com>
2026-03-03 00:57:09 -0700

    Update cosa.ps1

--- cosa.ps1
+++ cosa.ps1
@@ -1,3 +1,8 @@
-# Ver que sitios existen en IIS
-Import-Module WebAdministration
-Get-WebSite
+# Ver permisos exactos que tiene diego sobre su carpeta home
+icacls "C:\FTP\LocalUser\diego"
+
+$usuario = "diego"
+icacls "C:\FTP\LocalUser\$usuario" /grant "${usuario}:(OI)(CI)F" /T
+icacls "C:\FTP" /grant "${usuario}:(RX)"
+icacls "C:\FTP\LocalUser" /grant "${usuario}:(RX)"
+Restart-Service FTPSVC -Force
