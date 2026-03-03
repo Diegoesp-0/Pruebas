c0135b93cfc43ef70fca248b3ff8e05929cd3370
Espiritu <diegoesp1011@gmail.com>
2026-03-03 00:59:11 -0700

    Update cosa.ps1

--- cosa.ps1
+++ cosa.ps1
@@ -1,13 +1,5 @@
-$usuario = "diego"
-$equipo = $env:COMPUTERNAME
+# Ver a que grupos pertenece diego
+net user diego
 
-# Quitar herencia y aplicar permisos directos
-icacls "C:\FTP\LocalUser\$usuario" /inheritance:d
-icacls "C:\FTP\LocalUser\$usuario" /grant "${equipo}\${usuario}:(OI)(CI)F"
-icacls "C:\FTP\LocalUser\$usuario" /grant "Administrators:(OI)(CI)F"
-icacls "C:\FTP\LocalUser\$usuario" /grant "SYSTEM:(OI)(CI)F"
-
-# Verificar
-icacls "C:\FTP\LocalUser\$usuario"
-
-Restart-Service FTPSVC -Force
+# Si no esta en Usuarios, agregarlo
+net localgroup Usuarios diego /add
