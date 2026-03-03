a16e49088e183216b981f126d1f5cdd15d803e92
Espiritu <diegoesp1011@gmail.com>
2026-03-03 01:04:00 -0700

    Update cosa.ps1

--- cosa.ps1
+++ cosa.ps1
@@ -1,3 +1,5 @@
-# Ver como esta el sitio en el config real
-$config = [xml](Get-Content "C:\Windows\System32\inetsrv\config\applicationHost.config")
-$config.configuration.'system.applicationHost'.sites.site | Where-Object { $_.name -eq "FTP-Servidor" } | Select-Object -ExpandProperty ftpServer
+# Ver el XML completo del sitio FTP
+$configPath = "C:\Windows\System32\inetsrv\config\applicationHost.config"
+$config = [xml](Get-Content $configPath)
+$sitio = $config.configuration.'system.applicationHost'.sites.site | Where-Object { $_.name -eq "FTP-Servidor" }
+$sitio.OuterXml
