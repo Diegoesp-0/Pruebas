633ef279c18168c59bd94517dc8ad3f1f35ce618
Espiritu <diegoesp1011@gmail.com>
2026-03-03 01:15:27 -0700

    Update cosa.ps1

--- cosa.ps1
+++ cosa.ps1
@@ -1,6 +1,5 @@
 $configPath = "C:\Windows\System32\inetsrv\config\applicationHost.config"
-$content = Get-Content $configPath -Raw -Encoding UTF8
 
-# Mostrar la seccion del sitio FTP
-$match = [regex]::Match($content, '<site name="FTP-Servidor".*?</site>', [System.Text.RegularExpressions.RegexOptions]::Singleline)
-$match.Value
+# Leer y mostrar solo las lineas del sitio FTP
+$lines = Get-Content $configPath
+$lines | Select-String -Pattern "FTP-Servidor" -Context 5,5
