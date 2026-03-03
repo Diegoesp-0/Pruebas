71d73eb84656b0d3301602526ac1e1d86ff6da17
Espiritu <diegoesp1011@gmail.com>
2026-03-03 01:06:05 -0700

    Update cosa.ps1

--- cosa.ps1
+++ cosa.ps1
@@ -1,7 +1,20 @@
 Import-Module WebAdministration
-Set-ItemProperty "IIS:\Sites\FTP-Servidor" -Name ftpServer.userIsolation.mode -Value 3
-Restart-Service FTPSVC -Force
 
 $configPath = "C:\Windows\System32\inetsrv\config\applicationHost.config"
-(Get-Content $configPath -Raw) -replace 'mode="IsolateAllDirectories"', 'mode="IsolateRootDirectoryOnly"' | Set-Content $configPath
+
+# Cambiar a modo sin aislamiento
+(Get-Content $configPath -Raw) -replace 'mode="IsolateRootDirectoryOnly"', 'mode="None"' | Set-Content $configPath
+
+# La raiz del sitio es C:\FTP, el usuario diego necesita acceso ahi
+icacls "C:\FTP" /grant "diego:(OI)(CI)RX"
+icacls "C:\FTP\_general" /grant "diego:(OI)(CI)M"
+icacls "C:\FTP\_reprobados" /grant "diego:(OI)(CI)M"
+icacls "C:\FTP\_usuarios\diego" /grant "diego:(OI)(CI)F"
+
 Restart-Service FTPSVC -Force
+Start-Sleep -Seconds 2
+
+# Confirmar modo
+$config = [xml](Get-Content $configPath)
+$sitio = $config.configuration.'system.applicationHost'.sites.site | Where-Object { $_.name -eq "FTP-Servidor" }
+$sitio.ftpServer.userIsolation.mode
