80bbdd8f725e6c61eabdcd161d197e8affd8b4d2
Espiritu <diegoesp1011@gmail.com>
2026-03-05 21:30:34 -0700

    Update descargarftp.ps1

--- descargarftp.ps1
+++ descargarftp.ps1
@@ -1,50 +1,13 @@
-$configFile = "$env:SystemRoot\System32\inetsrv\config\applicationHost.config"
-[xml]$xml = Get-Content $configFile
-$xml.configuration.'system.applicationHost'.sites.site | 
-    Where-Object { $_.name -eq "ServidorFTP" } | 
-    Select-Object -ExpandProperty OuterXml
+Get-WindowsFeature Web-Ftp-Server, Web-Ftp-Service, Web-Ftp-Ext | 
+    Select Name, Installed, InstallState
 
+    sc.exe query ftpsvc
+sc.exe qc ftpsvc
 
-Import-Module WebAdministration
+# Ver logs de FTP en el visor de eventos (canal más específico)
+Get-WinEvent -ListLog "*ftp*","*iis*" -ErrorAction SilentlyContinue | 
+    Select LogName, RecordCount
 
-# Eliminar sitio corrupto
-& "$env:SystemRoot\System32\inetsrv\appcmd.exe" delete site "ServidorFTP"
-
-# Recrear limpio sin doble configuracion SSL
-New-WebFtpSite -Name "ServidorFTP" -Port 21 -PhysicalPath "C:\ftp" -Force
-
-# Isolation
-Set-ItemProperty "IIS:\Sites\ServidorFTP" -Name "ftpServer.userIsolation.mode" -Value 3
-
-# Autenticacion
-Set-ItemProperty "IIS:\Sites\ServidorFTP" `
-    -Name "ftpServer.security.authentication.basicAuthentication.enabled" -Value $true
-Set-ItemProperty "IIS:\Sites\ServidorFTP" `
-    -Name "ftpServer.security.authentication.anonymousAuthentication.enabled" -Value $true
-
-# SSL - solo UNA vez, con SslAllow
-Set-ItemProperty "IIS:\Sites\ServidorFTP" `
-    -Name "ftpServer.security.ssl.controlChannelPolicy" -Value "SslAllow"
-Set-ItemProperty "IIS:\Sites\ServidorFTP" `
-    -Name "ftpServer.security.ssl.dataChannelPolicy" -Value "SslAllow"
-
-# Autorizacion
-Clear-WebConfiguration "/system.ftpServer/security/authorization" `
-    -PSPath "IIS:\" -Location "ServidorFTP" -ErrorAction SilentlyContinue
-
-Add-WebConfiguration "/system.ftpServer/security/authorization" `
-    -PSPath "IIS:\" -Location "ServidorFTP" `
-    -Value @{ accessType="Allow"; users="?"; roles=""; permissions=1 }
-
-Add-WebConfiguration "/system.ftpServer/security/authorization" `
-    -PSPath "IIS:\" -Location "ServidorFTP" `
-    -Value @{ accessType="Allow"; users="*"; roles=""; permissions=3 }
-
-# Reiniciar y arrancar
-Stop-Service ftpsvc -Force
-Start-Sleep -Seconds 3
-Start-Service ftpsvc
-Start-Sleep -Seconds 3
-
-& "$env:SystemRoot\System32\inetsrv\appcmd.exe" start site "ServidorFTP"
-& "$env:SystemRoot\System32\inetsrv\appcmd.exe" list site "ServidorFTP"
+# Intentar leer el canal de IIS
+Get-WinEvent -LogName "Microsoft-IIS-FTPServer/Operational" -MaxEvents 10 `
+    -ErrorAction SilentlyContinue | Format-List TimeCreated, Message
