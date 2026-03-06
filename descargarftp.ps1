baa60aa9e64c5ae601f2905f3d4355cf6105c6ab
Espiritu <diegoesp1011@gmail.com>
2026-03-05 21:42:35 -0700

    Update descargarftp.ps1

--- descargarftp.ps1
+++ descargarftp.ps1
@@ -3,3 +3,5 @@ Get-ChildItem "C:\ftp\LocalUser\usuario1"
 Get-Acl "C:\ftp\LocalUser\usuario1" | Select -ExpandProperty Access | Format-Table
 
 Get-Acl "C:\ftp\LocalUser" | Select -ExpandProperty Access | Format-Table IdentityReference, FileSystemRights, AccessControlType
+
+Get-Acl "C:\ftp\LocalUser" | Select -ExpandProperty Access | Format-Table IdentityReference, FileSystemRights, AccessControlType
