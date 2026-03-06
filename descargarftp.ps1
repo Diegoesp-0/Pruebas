cf34a00dd83eb06db370079d60e68b1b231d893b
Espiritu <diegoesp1011@gmail.com>
2026-03-05 21:34:23 -0700

    Update descargarftp.ps1

--- descargarftp.ps1
+++ descargarftp.ps1
@@ -1,13 +1,3 @@
-Install-WindowsFeature Web-Ftp-Ext -IncludeManagementTools
-
-# Verificar
-Get-WindowsFeature Web-Ftp-Server, Web-Ftp-Service, Web-Ftp-Ext | 
-    Select Name, Installed, InstallState
-
-    Stop-Service ftpsvc -Force
-Start-Sleep -Seconds 3
-Start-Service ftpsvc
-Start-Sleep -Seconds 3
-
+# Iniciar sitio
 & "$env:SystemRoot\System32\inetsrv\appcmd.exe" start site "ServidorFTP"
 & "$env:SystemRoot\System32\inetsrv\appcmd.exe" list site "ServidorFTP"
